d8ee34066d654653bc9bb4c5fb48b1077d213bc5
Diego Porras (daporr) <diego.porras@owasp.org>
2021-07-14 00:13:50 -0600

    Feature: Add support for switching directory context ie. cd command

--- editor/api/terminal/business.py
+++ editor/api/terminal/business.py
@@ -1,18 +1,31 @@
-import subprocess, shlex, time
+import subprocess, shlex, time, os
+context = {
+    "user":os.getenv("USER"),
+    "cwd": os.getenv("PWD")
+}
 
+def chdir(cmd):
+    new_dir = HOME_DIR =  os.getenv("HOME")
+    if len(cmd) > 1:
+        new_dir = cmd[1].replace("~", HOME_DIR)
+    os.chdir(new_dir)
+    context['cwd'] = os.getcwd()
 
 def get_terminal_output(cmd):
     named_tuple = time.localtime() # get struct_time
     current_time = time.strftime("%H:%M:%S", named_tuple)
+    output_cmd = ""
     try:
-        result = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE)
-        output_cmd = result.stdout.decode('utf-8')
+        cmd = shlex.split(cmd)
+        if cmd[0] == 'cd':
+            chdir(cmd)
+        else:
+            result = subprocess.run(cmd, stdout=subprocess.PIPE)
+            output_cmd = result.stdout.decode('utf-8')
     except Exception:
         output_cmd = "Error running command"
-    path = subprocess.run('pwd', stdout=subprocess.PIPE)
-    output_pwd = path.stdout.decode('utf-8')
     result = '<div class="commandLine"><div class="user">&nbsp;&nbsp; app &nbsp;</div>'+\
-             '<div class="cwd">&nbsp;' +str(output_pwd)+ '&nbsp;</div> : ' +str(current_time)+ \
+             '<div class="cwd">&nbsp;' +str(context['cwd'])+ '&nbsp;</div> : ' +str(current_time)+ \
              '<br>'+\
              '<div class="promptVLine"></div><div class="promptHLine">─<div class="promptArrow">▶</div></div> ' +str(cmd)+ '</div></div><br> ' +str(output_cmd)
-    return {'output': ''+str(result)+'', 'user': 'app', 'cwd':''+str(output_pwd)+''} 
+    return {'output': ''+str(result)+'', 'user': 'app', 'cwd':str(context['cwd'])} 
