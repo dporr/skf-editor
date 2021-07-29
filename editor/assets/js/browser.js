b189ed32d8006eaf385a0ff338928f424d6b6de6
Diego Porras (daporr) <diego.porras@owasp.org>
2021-07-28 19:31:28 -0600

    Add history functinality #GSoC21 #SKF #OWASP

--- editor/assets/js/browser.js
+++ editor/assets/js/browser.js
@@ -1,3 +1,7 @@
+//Keep the current state for the history
+history = [];
+history_pointer = 0;
+
 //Allow child iframe to report its URL
 window.addEventListener("message", function(event) {
     if (!window.location.origin) {
@@ -18,4 +22,19 @@ function openUrl(){
     var browserIframe = document.getElementById('browser-iframe');
     //TODO: Validate URL: protocol + FQDN + [:port] + query_string
     browserIframe.src = urlbar.value; //baking our own XSS lol
+}
+
+function browseHistory(direction){
+  switch(direction){
+    case 'forward':
+      if(history_pointer < (history.length - 1)) history_pointer++;
+      break;
+    case 'back':
+      if(history_pointer != 0) history_pointer--;
+      break;
+    default:
+      return;
+  }
+  updateAddressBar(history_pointer[history_pointer]);
+  openUrl();
 }
